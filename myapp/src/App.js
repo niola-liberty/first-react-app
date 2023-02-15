4147e437f92755a78dc2118d9a3b8b5a885c0b71
niola-liberty <82907562+niola-liberty@users.noreply.github.com>
2023-02-15 14:32:03 +0100

    finial commit: awaiting deployment

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,30 +1,57 @@
-import {useEffect} from 'react';
+import { useEffect, useState} from 'react';
 import searchIcon from './search.svg';
 import './App.css';
+import MovieCard from './moviecard';
 
 // 
 const API_URL = "http://www.omdbapi.com?apikey=baa6eab2";
 
+const movie1 = {
+    "Title": "Batman v Superman: Dawn of Justice",
+    "Year": "2016",
+    "imdbID": "tt2975590",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+}
+
 function App() {
+  const [movies, setMovies]= useState([]);
+  const [searchTerm, setSearchTerm] = useState('')
+
   const searchMovies = async (title) => {
     const response =await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search);  
+    setMovies(data.Search);  
   }
- useEffect(() => {
+ useEffect(() => { 
     searchMovies('Superman')
  }, []);
   return (
     <div className="app">
       <h1>NetPlay </h1>
       <div className = "search">
-        <input placeholder='Search for movies' value="Superman" ></input>
-        <img src={searchIcon} onClick></img>
+        <input placeholder='Search for movies' value={searchTerm} onChange = {(e) => setSearchTerm(e.target.value)}></input>
+        <img src={searchIcon} onClick = {() => searchMovies(searchTerm)}></img>
       </div>
-      <div className ="container">
-         
-      </div>
+
+      {
+        movies?.length > 0 ?(
+          <div className ="container">
+           {movies.map((movie) =>(
+              <MovieCard movie = {movie} />
+            ))}
+
+          </div>  
+        ) :(
+          <div className='empty'>
+            <h2>No movies found</h2>
+          </div>
+        )
+      }
+
+      
     </div>
+    
   );
 }
 
